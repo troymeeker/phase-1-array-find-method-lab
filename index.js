bf35ac14fcbcbe69d07e5dafaa4965dd947659ec
troymeeker <troymeeker1@gmail.com>
2021-07-15 11:39:27 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(records){      
+  const winFinder = records.find(record => record.result === "W")
+  //const found = array1.find(element => element > 10);
+    if (winFinder === undefined){
+        return undefined;
+    } else {
+        return winFinder.year;
+    }
+}
+  
